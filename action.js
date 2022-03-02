52f10d02ebe914d9472d504b868c9a9bcee70d91
ganimtron-10 <praneethshetty10@gmail.com>
2022-03-02 16:14:15 +0530

    adding test code

--- action.js
+++ action.js
@@ -1,71 +1,237 @@
-let ball;
-let box;
-let spike;
-let boxes;
+// let ball;
+// let box;
+// let spike;
+// let boxes;
+
+// function setup() {
+//   createCanvas(300, 600);
+
+//   ball = createSprite(150,50,30,30);
+//   ball.draw = function(){fill(255,0,0);circle(0,0,20,20);}
+//   ball.velocity.y = 3;
+
+
+
+//   boxes = new Group();
+
+//   wall_left = createSprite(0,height/2,20,height)
+//   wall_left.immovable = true
+//   boxes.add(wall_left)
+
+//   wall_left = createSprite(width,height/2,20,height)
+//   wall_left.immovable = true
+//   boxes.add(wall_left)
+
+//   let y_pos = 300;
+
+//   for(let i = 0; i < 9; i++){
+//     y_pos += 80;
+
+//     new_box = createSprite(random(50,width-50),y_pos,random(50,100),20)
+//     new_box.velocity.y = -3;
+//     new_box.immovable = true;
+//     boxes.add(new_box);
+//   }
+
+//   // spike = new Group();
+// }
+
+// function draw() {
+//   background(220);
+
+//   if(ball.collide(boxes)){
+//     ball.velocity.y = 0;
+//   }else{
+//     ball.velocity.y = 3;
+//   }
+
+//   for(let i = 0; i < boxes.length; i++){
+//     if(boxes[i].position.y < -100){
+//       boxes[i].position.x = random(50,width-50);
+//       boxes[i].position.y = height;
+//     }
+//   }
+
+//   if(keyIsDown(LEFT_ARROW)){
+//     ball.velocity.x = -4;
+//   }else if(keyIsDown(RIGHT_ARROW)){
+//     ball.velocity.x = 4;
+//   }else{
+//     ball.velocity.x = 0;
+//   }
+
+//   if(ball.position.y > height || ball.position.y < 0){
+//     ball.remove()
+//     console.log("Ball removed!");
+//     noLoop();
+//   }
+
+//   drawSprites();
+
+// }
+
+
+// the snake is divided into small segments, which are drawn and edited on each 'draw' call
+let numSegments = 10;
+let direction = 'right';
+
+const xStart = 0; //starting x coordinate for snake
+const yStart = 250; //starting y coordinate for snake
+const diff = 10;
+
+let xCor = [];
+let yCor = [];
+
+let xFruit = 0;
+let yFruit = 0;
+let scoreElem;
 
 function setup() {
-  createCanvas(300, 600);
+  scoreElem = createDiv('Score = 0');
+  scoreElem.position(20, 20);
+  scoreElem.id = 'score';
+  scoreElem.style('color', 'white');
 
-  ball = createSprite(150,50,30,30);
-  ball.draw = function(){fill(255,0,0);circle(0,0,20,20);}
-  ball.velocity.y = 3;
+  createCanvas(500, 500);
+  frameRate(15);
+  stroke(255);
+  strokeWeight(10);
+  updateFruitCoordinates();
 
-
-
-  boxes = new Group();
-
-  wall_left = createSprite(0,height/2,20,height)
-  wall_left.immovable = true
-  boxes.add(wall_left)
-
-  wall_left = createSprite(width,height/2,20,height)
-  wall_left.immovable = true
-  boxes.add(wall_left)
-
-  let y_pos = 300;
-
-  for(let i = 0; i < 9; i++){
-    y_pos += 80;
-
-    new_box = createSprite(random(50,width-50),y_pos,random(50,100),20)
-    new_box.velocity.y = -3;
-    new_box.immovable = true;
-    boxes.add(new_box);
+  for (let i = 0; i < numSegments; i++) {
+    xCor.push(xStart + i * diff);
+    yCor.push(yStart);
   }
-
-  // spike = new Group();
 }
 
 function draw() {
-  background(220);
-
-  if(ball.collide(boxes)){
-    ball.velocity.y = 0;
-  }else{
-    ball.velocity.y = 3;
+  background(0);
+  for (let i = 0; i < numSegments - 1; i++) {
+    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
   }
+  updateSnakeCoordinates();
+  checkGameStatus();
+  checkForFruit();
+}
 
-  for(let i = 0; i < boxes.length; i++){
-    if(boxes[i].position.y < -100){
-      boxes[i].position.x = random(50,width-50);
-      boxes[i].position.y = height;
+/*
+ The segments are updated based on the direction of the snake.
+ All segments from 0 to n-1 are just copied over to 1 till n, i.e. segment 0
+ gets the value of segment 1, segment 1 gets the value of segment 2, and so on,
+ and this results in the movement of the snake.
+
+ The last segment is added based on the direction in which the snake is going,
+ if it's going left or right, the last segment's x coordinate is increased by a
+ predefined value 'diff' than its second to last segment. And if it's going up
+ or down, the segment's y coordinate is affected.
+*/
+function updateSnakeCoordinates() {
+  for (let i = 0; i < numSegments - 1; i++) {
+    xCor[i] = xCor[i + 1];
+    yCor[i] = yCor[i + 1];
+  }
+  switch (direction) {
+    case 'right':
+      xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
+      yCor[numSegments - 1] = yCor[numSegments - 2];
+      break;
+    case 'up':
+      xCor[numSegments - 1] = xCor[numSegments - 2];
+      yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
+      break;
+    case 'left':
+      xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
+      yCor[numSegments - 1] = yCor[numSegments - 2];
+      break;
+    case 'down':
+      xCor[numSegments - 1] = xCor[numSegments - 2];
+      yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
+      break;
+  }
+}
+
+/*
+ I always check the snake's head position xCor[xCor.length - 1] and
+ yCor[yCor.length - 1] to see if it touches the game's boundaries
+ or if the snake hits itself.
+*/
+function checkGameStatus() {
+  if (
+    xCor[xCor.length - 1] > width ||
+    xCor[xCor.length - 1] < 0 ||
+    yCor[yCor.length - 1] > height ||
+    yCor[yCor.length - 1] < 0 ||
+    checkSnakeCollision()
+  ) {
+    noLoop();
+    const scoreVal = parseInt(scoreElem.html().substring(8));
+    scoreElem.html('Game ended! Your score was : ' + scoreVal);
+  }
+}
+
+/*
+ If the snake hits itself, that means the snake head's (x,y) coordinate
+ has to be the same as one of its own segment's (x,y) coordinate.
+*/
+function checkSnakeCollision() {
+  const snakeHeadX = xCor[xCor.length - 1];
+  const snakeHeadY = yCor[yCor.length - 1];
+  for (let i = 0; i < xCor.length - 1; i++) {
+    if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
+      return true;
     }
   }
+}
 
-  if(keyIsDown(LEFT_ARROW)){
-    ball.velocity.x = -4;
-  }else if(keyIsDown(RIGHT_ARROW)){
-    ball.velocity.x = 4;
-  }else{
-    ball.velocity.x = 0;
+/*
+ Whenever the snake consumes a fruit, I increment the number of segments,
+ and just insert the tail segment again at the start of the array (basically
+ I add the last segment again at the tail, thereby extending the tail)
+*/
+function checkForFruit() {
+  point(xFruit, yFruit);
+  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
+    const prevScore = parseInt(scoreElem.html().substring(8));
+    scoreElem.html('Score = ' + (prevScore + 1));
+    xCor.unshift(xCor[0]);
+    yCor.unshift(yCor[0]);
+    numSegments++;
+    updateFruitCoordinates();
   }
+}
 
-  if(ball.position.y > height || ball.position.y < 0){
-    ball.remove()
-    console.log("Ball removed!");
-    noLoop();
+function updateFruitCoordinates() {
+  /*
+    The complex math logic is because I wanted the point to lie
+    in between 100 and width-100, and be rounded off to the nearest
+    number divisible by 10, since I move the snake in multiples of 10.
+  */
+
+  xFruit = floor(random(10, (width - 100) / 10)) * 10;
+  yFruit = floor(random(10, (height - 100) / 10)) * 10;
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 74:
+      if (direction !== 'right') {
+        direction = 'left';
+      }
+      break;
+    case 76:
+      if (direction !== 'left') {
+        direction = 'right';
+      }
+      break;
+    case 73:
+      if (direction !== 'down') {
+        direction = 'up';
+      }
+      break;
+    case 75:
+      if (direction !== 'up') {
+        direction = 'down';
+      }
+      break;
   }
-
-  drawSprites();
-
 }
